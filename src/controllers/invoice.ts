7f2b06f63ea5fabc53d359a76fcba67c2efc2760
Nahuel <lescanoagustin10nahuel@gmail.com>
2026-06-14 19:42:41 -0300

    fix: uso el modelo de gemini 2.5 flash lite

--- src/controllers/invoice.ts
+++ src/controllers/invoice.ts
@@ -27,7 +27,7 @@ export const parseInvoice = async (req: Request, res: Response) => {
   let responseAI;
   try {
     responseAI = await ai.models.generateContent({
-      model: "gemini-1.5-flash",
+      model: "gemini-2.5-flash-lite",
       contents: [
         prompt,
         {
