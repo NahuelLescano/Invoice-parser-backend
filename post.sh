0e38e9479c17682988663376fb7727c1a1a12000
Nahuel <lescanoagustin10nahuel@gmail.com>
2026-06-15 12:59:59 -0300

    fix: indetation

--- post.sh
+++ post.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-IMAGE_PATH="./images/factura.jpeg"
+IMAGE_PATH="./images/quilmes.jpeg"
 PAYLOAD_FILE="payload_tmp.json"
 
 if [ ! -f "$IMAGE_PATH" ]; then
-    echo "Error: No se encontró el archivo '$IMAGE_PATH'."
-    exit 1
+  echo "Error: No se encontró el archivo '$IMAGE_PATH'."
+  exit 1
 fi
 
 echo "Generando payload JSON..."
 
 # 1. Armamos el JSON escribiendo directamente en un archivo temporal
-echo -n '{"mimeType": "image/jpeg", "imageBase64": "' > "$PAYLOAD_FILE"
+echo -n '{"mimeType": "image/jpeg", "imageBase64": "' >"$PAYLOAD_FILE"
 # 2. Inyectamos el base64 directamente en el archivo
-base64 -w 0 "$IMAGE_PATH" >> "$PAYLOAD_FILE"
+base64 -w 0 "$IMAGE_PATH" >>"$PAYLOAD_FILE"
 # 3. Cerramos el JSON
-echo '"}' >> "$PAYLOAD_FILE"
+echo '"}' >>"$PAYLOAD_FILE"
 
 echo "Enviando petición al backend..."
 
